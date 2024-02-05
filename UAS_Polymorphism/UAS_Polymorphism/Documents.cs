ab23ae66b48fd058c2042685b29ae8e600047123
hanifahaksiomatika <128040561+hanifahaksiomatika@users.noreply.github.com>
2024-02-06 05:39:03 +0700

    Add files via upload

---
+++ UAS_Polymorphism/UAS_Polymorphism/Documents.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_Polymorphism
+{
+    public class Documents
+    {
+        public string Name { get; set; }    // properties
+
+        // method
+        /*public virtual void Open()
+        {
+            Console.WriteLine("Membuka Document...");
+        }*/
+        public virtual void Save()
+        {
+            Console.WriteLine("Save Document\n");
+        }
+    }
+    public class Word : Documents
+    {
+        /*public override void Open()
+        {
+            Console.WriteLine("Membuka Document Word...");
+        }*/
+        public override void Save()
+        {
+            Console.WriteLine("Document: .docx tersimpan");
+        }
+    }
+    public class Excel : Documents
+    {
+        /*public override void Open()
+        {
+            Console.WriteLine("Membuka Document Excel...");
+        }*/
+        public override void Save()
+        {
+            Console.WriteLine("Document: .xls tersimpan");
+        }
+    }
+    public class PowerPoint : Documents
+    {
+        /*public override void Open()
+        {
+            Console.WriteLine("Membuka Document PowerPoint...");
+        }*/
+        public override void Save()
+        {
+            Console.WriteLine("Document: .ppt tersimpan");
+        }
+    }
+}
---
+++ UAS_Polymorphism/UAS_Polymorphism/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Documents documents;
+
+            Console.WriteLine("Pilih Document Anda: ");
+            Console.WriteLine("1. Word");
+            Console.WriteLine("2. Excel");
+            Console.WriteLine("3. Power Point");
+
+            Console.WriteLine("\nNomor Document [1..3]: ");
+            int nomorDocument = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorDocument == 1)
+                documents = new Word();
+            else if (nomorDocument == 2)
+                documents = new Excel();
+            else
+                documents = new PowerPoint();
+
+            documents.Open();
+            documents.Save();*/
+
+            Documents documents = new Documents();
+            documents.Save();
+
+            Word word = new Word();
+            documents = word;              // polymorphism
+            //word.Save();                 // polymorphism
+            documents.Save();              // polymorphism
+
+
+            Excel excel = new Excel();
+            documents = excel;             // polymorphism
+            //excel.Save();                // polymorphism
+            documents.Save();              // polymorphism
+
+            PowerPoint powerpoint = new PowerPoint();
+            documents = powerpoint;        // polymorphism
+            //powerpoint.Save();           // polymorphism
+            documents.Save();              // polymorphism
+            
+
+            Console.ReadKey();
+        }
+    }
+}
